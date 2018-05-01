1b2d9221e815e249ee41cd908463a7eb6b8b73b3
yonig <yonigev@gmail.com>
2018-05-01 19:18:45 +0300

    Q4,6

--- q4.rkt
+++ q4.rkt
@@ -1,32 +1,58 @@
 #lang racket
 (provide (all-defined-out))
 
-;; Add a contract here
+;; Signature: shift-left(ls)
+;; Type: [ List(T) -> List(T) ]
+;; Purpose: takes a list as an argument and shifts the list one time to the left.
 (define shift-left
   (lambda (ls)
-    "Implement me"))
+    (if (empty? ls)
+    '()
+    (append (cdr ls) (list (first ls ))))))
+    
 
-;; Add a contract here
+;; Signature: shift-k-left(ls k)
+;; Type: [ List(T) -> List(T) ]
+;; Purpose: takes a list and a number k ≥ 0 and "shift-left" the list k times.
 (define shift-k-left
   (lambda (ls k)
-    "Implement me"))
+    (cond ((equal? k 0) ls)
+          ((empty? ls) ls)
+          (else  (shift-k-left (shift-left ls) (- k 1))))))
 
-;; Add a contract here
+;; Signature: shift-right(ls)
+;; Type: [ List(T) -> List(T) ]
+;; Purpose: takes a list as an argument and evaluates the list that is its’ shift-right by one place.
 (define shift-right
   (lambda (ls)
-    "Implement me"))
+    (shift-k-left ls (- (length ls) 1))))
+  ;;  (cond ((empty? ls)'())
+   ;;     (else (append (cons (last ls) `()) (take ls (- (length ls) 1)))))))
+    
 
-;; Add a contract here
+;; Signature: combine(ls1 ls2)
+;; Type: [ List*List -> List ]
+;; Purpose: takes two lists and combines them in to one new list in an alternating manner starting from the first one.
 (define combine
   (lambda (ls1 ls2)
-    "Implement me"))
+    (cond ((empty? ls1) ls2)
+          ((empty? ls2) ls1)
+          (else (append (cons (first ls1) `()) (cons (first ls2) (combine(cdr ls1)(cdr ls2))))))))
 
-;; Add a contract here
+;; Signature: sum-tree(tree)
+;; Type: [((List(Number) union Number) -> Number]
+;; Purpose: receives a tree which has data values in his nodes which are all non-negetive numbers and return the sum of nodes values.
 (define sum-tree
   (lambda (tree)
-    "Implement me"))
+    (cond ((number? tree) tree)  ;; if it's a number, return it. else "reduce" it using foldl     
+          ((empty? tree) 0)           
+          (else (foldl + 0 (map (lambda (x) (sum-tree x)) tree))))))
 
-;; Add a contract here
-(define inverse-tree
+;; Signature: inverse-tree (tree)
+;; Type: [((List(Number| Boolean) | Number | Boolean) -> ((List(Number| Boolean) | Number | Boolean)]
+;; Purpose: receives a tree which has number and boolean values, and inverses each node's value.
   (lambda (tree)
-    "Implement me"))
+    (cond ((empty? tree) `())
+          ((number? tree)  (* -1 tree))
+          ((boolean? tree) (not tree))
+          (else (map (lambda (x) (inverse-tree x)) tree)))))
---
+++ q6.ts
@@ -0,0 +1,34 @@
+import { CExp, Exp, PrimOp, Program, DefineExp } from "./L1-ast";
+import { hasError, isAppExp, isBoolExp, isCExp, isDefineExp, isError, isNumExp, isPrimOp,
+         isProgram, isVarRef, isVarDecl } from "./L1-ast";
+import { parseL1 } from "./L1-ast";
+import { first, isEmpty, rest } from "./L1-ast";
+import * as assert from "assert";
+import { filter, map, reduce } from "ramda";
+import {getErrorMessages} from "./L3-ast";
+// Implement the following function:
+export const unparse = (x: Program | DefineExp | CExp) : string | Error => 
+           isProgram(x)?  "(L1 "+map(unparse,x.exps).join(" ")+")":
+           isDefineExp(x)? "("+"define "+unparse(x.var)+" "+unparse(x.val)+")":
+           isAppExp(x)? "("+unparse(x.rator)+" "+map(unparse,x.rands).join(" ")+")":
+           isBoolExp(x)? x.val === true ? "#t": "#f" :
+           isNumExp(x)? x.val.toString():
+           isPrimOp(x)? x.op.toString():
+           isVarRef(x)? x.var.toString():
+           isVarDecl(x)? x.var.toString():             
+           x.message.toString()                         //ERROR
+           
+
+           
+
+
+console.log(unparse(parseL1("1")));
+
+console.log(unparse(parseL1("#t")));
+
+console.log(unparse(parseL1("#f")));
+
+
+console.log(unparse(parseL1("(+ x 5)")));
+
+ console.log(unparse(parseL1("(L1     (define x 5)     (+ x 5)     (+ (+ (- x y) 3) 4)     (and #t x))")));
